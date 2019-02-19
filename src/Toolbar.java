7e347215ecce19af54529954779d8e2c7194f556
John Woodell <woodie@netpress.com>
2019-02-19 14:30:15 -0800

    Toolbar swop icon two pixels shorter.

--- src/Toolbar.java
+++ src/Toolbar.java
@@ -31,21 +31,21 @@ class Toolbar {
   * Draw the swop icon from a center point.
   */
   public static void drawSwopIcon(Graphics g, int cx, int cy)  {
-    g.drawLine(cx - 10, cy - 6, cx - 7, cy - 9);
     g.drawLine(cx - 10, cy - 5, cx - 7, cy - 8);
-    g.drawLine(cx - 9, cy - 5, cx - 7, cy - 7);
-    g.drawRect(cx - 6, cy - 10, 1, 19);
-    g.drawLine(cx - 4, cy - 9, cx - 1, cy - 6);
+    g.drawLine(cx - 10, cy - 4, cx - 7, cy - 7);
+    g.drawLine(cx - 9, cy - 4, cx - 7, cy - 6);
+    g.drawRect(cx - 6, cy - 9, 1, 17);
     g.drawLine(cx - 4, cy - 8, cx - 1, cy - 5);
-    g.drawLine(cx - 4, cy - 7, cx - 2, cy - 5);
+    g.drawLine(cx - 4, cy - 7, cx - 1, cy - 4);
+    g.drawLine(cx - 4, cy - 6, cx - 2, cy - 4);
 
-    g.drawLine(cx + 0, cy + 4, cx + 3, cy + 6);
-    g.drawLine(cx + 0, cy + 5, cx + 3, cy + 7);
-    g.drawLine(cx + 1, cy + 4, cx + 3, cy + 8);
-    g.drawRect(cx + 4, cy - 10, 1, 19);
-    g.drawLine(cx + 6, cy + 6, cx + 8, cy + 4);
+    g.drawLine(cx + 0, cy + 3, cx + 3, cy + 6);
+    g.drawLine(cx + 0, cy + 4, cx + 3, cy + 7);
+    g.drawLine(cx + 1, cy + 3, cx + 3, cy + 5);
+    g.drawRect(cx + 4, cy - 9, 1, 17);
+    g.drawLine(cx + 6, cy + 5, cx + 8, cy + 3);
+    g.drawLine(cx + 6, cy + 6, cx + 9, cy + 3);
     g.drawLine(cx + 6, cy + 7, cx + 9, cy + 4);
-    g.drawLine(cx + 6, cy + 8, cx + 9, cy + 5);
   }
 
 }

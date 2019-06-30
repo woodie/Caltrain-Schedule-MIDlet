f2a17ea4adb98068c6677b3a07b0419a07e22d59
John Woodell <woodie@netpress.com>
2019-06-30 00:14:24 -0700

    Use microemulator which displays reasonable fonts.

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+ME2=/Library/Java/microemulator-2.0.4
 JDK=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 JME=/Applications/Java_ME_SDK_3.0.app/Contents/Resources
 
 CLDC_1_1=$JME/lib/cldc_1.1.jar
 MIDP_2_0=$JME/lib/midp_2.0.jar
 
+DEVICE=$ME2/devices/microemu-device-large.jar
+APP_NAME=NextCaltrain
+
 VER=$(grep MIDlet-Version MANIFEST.MF | cut -d' ' -f 2)
-APP=NextCaltrain-$VER
+APP=$APP_NAME-$VER
 
 rm -rf tmpclasses classes dist
 mkdir -p tmpclasses classes dist
@@ -29,8 +33,10 @@ MIDlet-Jar-Size: $(stat -f %z dist/$APP.jar)" >> dist/$APP.jad
 cd dist
 zip -r $APP.zip $APP.jad $APP.jar
 cd ..
- 
+
+# https://code.google.com/archive/p/microemu/
 if [ -f dist/$APP.jad ]; then
-  $JME/bin/emulator -Xdevice:DefaultCldcPhone1 \
-      -Xdescriptor:dist/$APP.jad -Xdomain:maximum
+  $JDK/bin/java -cp $ME2/microemulator.jar:$DEVICE:dist/$APP.jar \
+      org.microemu.app.Main $APP_NAME --propertiesjad dist/$APP.jad \
+      --device org/microemu/device/large/device.xml
 fi
